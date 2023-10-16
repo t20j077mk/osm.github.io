61ad05519f365c789ee5c3205c4e72b3944df4cb
t20j077mk <114904360+t20j077mk@users.noreply.github.com>
2023-10-16 11:51:29 +0900

    Update route.js

--- route.js
+++ route.js
@@ -1,14 +1,14 @@
 function init() {
     var Basic_Map = new Array();
-     Basic_Map[ 0 ] = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     Basic_Map[ 0 ] = L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
        continuousWorld: false
     });
-   var map_104 = L.map('map_104').setView([35.65809922, 139.74135747], 8);
+   var map_104 = L.map('map_104').setView([34.694925577318706,133.92794107726027], 13);
    map_104.addLayer( Basic_Map[ 0 ] );
 
    var baseMap = {
-   "OpenStreetMap": Basic_Map[ 0 ],
+   "Cyclemap": Basic_Map[ 0 ],
    };
 
    L.control.layers(baseMap).addTo(map_104);
